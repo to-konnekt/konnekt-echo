fed6377ad4dcc621bb6bc856ea5995da790e5e53
telegine <telegine@yandex-team.ru>
2024-12-11 02:48:07 +0300

    feat: add test-udp-server that sends neiry data to nodejs server

---
+++ server/udp-test-server.js
@@ -0,0 +1,86 @@
+import dgram from 'dgram';
+import { TEST_SERVER_OUT_HOST, TEST_SERVER_OUT_PORT, TEST_SERVER_IN_HOST, TEST_SERVER_IN_PORT, UDP_PORT, UDP_HOST } from '../shared/constants.js';
+
+const serverOut = dgram.createSocket('udp4');
+const serverIn = dgram.createSocket('udp4');
+
+serverOut.on('message', (msg, rinfo) => {
+    console.log(`ServerOut received: ${msg} from ${rinfo.address}:${rinfo.port}`);
+
+    const responseMessage = Buffer.from('Hello, client!');
+    serverOut.send(responseMessage, rinfo.port, rinfo.address, (err) => {
+        if (err) {
+            console.error('Error sending response:', err);
+        } else {
+            console.log('Response sent to client');
+        }
+    });
+});
+
+serverOut.on('listening', () => {
+    const address = serverOut.address();
+    console.log(`ServerOut listening on ${address.address}:${address.port}`);
+});
+
+serverOut.bind(TEST_SERVER_OUT_PORT, TEST_SERVER_OUT_HOST);
+
+serverOut.on('error', (err) => {
+    console.error(`ServerOut error:\n${err.stack}`);
+    serverOut.close();
+});
+
+process.on('SIGINT', () => {
+    console.log('Closing serverOut...');
+    serverOut.close(() => {
+        console.log('Server closed');
+        process.exit(0);
+    });
+});
+
+setInterval(() => {
+    const neiryParam1 = '1';
+    const neiryParam2 = '9';
+
+    const messageString = neiryParam1 + ' ' + neiryParam2;
+    const message = Buffer.from(messageString);
+    serverOut.send(message, UDP_PORT, UDP_HOST, (err) => {
+        if (err) {
+            console.error('Error sending response:', err);
+        } else {
+            console.log(`Message "${messageString}" sent to client ${UDP_HOST}:${UDP_PORT}`);
+        }
+    })
+}, 1000)
+
+serverIn.on('message', (msg, rinfo) => {
+    console.log(`ServerIn received: ${msg} from ${rinfo.address}:${rinfo.port}`);
+
+    const responseMessage = Buffer.from('Hello, client!');
+    serverIn.send(responseMessage, rinfo.port, rinfo.address, (err) => {
+        if (err) {
+            console.error('Error sending response:', err);
+        } else {
+            console.log('Response sent to client');
+        }
+    });
+});
+
+serverIn.on('listening', () => {
+    const address = serverIn.address();
+    console.log(`ServerIn listening on ${address.address}:${address.port}`);
+});
+
+serverIn.bind(TEST_SERVER_IN_PORT, TEST_SERVER_IN_HOST);
+
+serverIn.on('error', (err) => {
+    console.error(`ServerIn error:\n${err.stack}`);
+    serverIn.close();
+});
+
+process.on('SIGINT', () => {
+    console.log('Closing serverIn...');
+    serverIn.close(() => {
+        console.log('Server closed');
+        process.exit(0);
+    });
+});
--- shared/constants.js
+++ shared/constants.js
@@ -1,13 +1,28 @@
 export const SCREENS = [
-  { id: "screen1", name: "Screen 1" },
-  { id: "screen2", name: "Screen 2" },
-  { id: "screen3", name: "Screen 3" },
-  { id: "screen4", name: "Screen 4" },
+  { id: "idle", name: "Idle" },
+  { id: "intro", name: "Intro" },
+  { id: "action", name: "Action" },
+  { id: "answer", name: "Answer" },
+  { id: "end", name: "End" }
 ];
+
+export const SCREENS_TRANSITIONS = {
+  'idle': 'intro',
+  'intro': 'action',
+  'action': 'answer',
+  'answer': 'end',
+  'end': 'idle'
+};
 
 export const WS_PORT = 3001;
 export const UDP_PORT = 5000;
+export const UDP_HOST = '127.0.0.1';
 
 export const MESSAGE_TYPE = {
   CHANGE_SCREEN: "CHANGE_SCREEN",
 };
+
+export const TEST_SERVER_OUT_PORT = 4444;
+export const TEST_SERVER_OUT_HOST = '127.0.0.1';
+export const TEST_SERVER_IN_PORT = 5555;
+export const TEST_SERVER_IN_HOST = '127.0.0.1';
