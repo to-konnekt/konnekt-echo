a244f112a2f6fbdd38c26d376ee23f27a4c5b863
telegine <telegine@yandex-team.ru>
2024-12-11 05:16:32 +0300

    feat: proxy to real backends

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async rewrites() {
+        return [
+            {
+                source: '/api/:path*',
+                destination: 'http://zabelin-bot.llmplay.ru:8000/:path*',
+            },
+            {
+                source: '/cloud/api/:path*',
+                destination: 'https://llm.api.cloud.yandex.net/:path*',
+            },
+            {
+                source: '/speechkit/api/:path*',
+                destination: 'https://tts.api.cloud.yandex.net/:path*'
+            }
+        ]
+    },
+    reactStrictMode: false
+};
 
 export default nextConfig;
